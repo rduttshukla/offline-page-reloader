9e96cd315fb9616b6895e823184f8423cb09ddc8
Rishi Dutt Shukla <duttrishi72@gmail.com>
2023-04-16 01:19:23 -0500

    Replace faulty logging

--- content-script.js
+++ content-script.js
@@ -1,7 +1,9 @@
-console.log('This is a messsage from the extension.')
-
 // Listen for the 'online' event to detect when the browser goes back online
 window.addEventListener('online', () => {
-    consoleToTheCurrentTab('browser is back online')
     browser.runtime.sendMessage({ action: "reloadCurrentTab" });
+});
+
+// Listen for the 'offline' event to detect when the browser goes offline
+window.addEventListener('offline', () => {
+    console.log('Auto Reload: Browser is now offline')
 });
--- utils.js
+++
@@ -1,6 +0,0 @@
-const consoleToTheCurrentTab = (tab, message) => {
-    browser.tabs.sendMessage(tab.id, {
-        action: 'injectMessage',
-        message
-    });
-}
